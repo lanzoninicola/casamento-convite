0b27048b25df597699db5656b84578997f70496e
Nicola <lanzoni.nicola@gmail.com>
2022-03-31 16:00:50 -0300

    Added comments

--- app/lib/firebase/firestore.service.ts
+++ app/lib/firebase/firestore.service.ts
@@ -1,22 +1,46 @@
 import {
-  Firestore,
-  collection,
-  doc,
   addDoc,
-  getDocs,
-  getDoc,
+  collection,
   deleteDoc,
+  doc,
+  Firestore,
+  getDoc,
+  getDocs,
   updateDoc,
 } from "firebase/firestore";
+
 import {
+  FirestoreAdditionSuccessResponse,
   FirestoreCollectionResponse,
   FirestoreCRUDService,
   FirestoreDocumentResponse,
+  FirestoreErrorResponse,
+  FirestoreSuccessResponse,
 } from "./firestore.interfaces";
 
 export default class FirestoreService implements FirestoreCRUDService {
   constructor(private db: Firestore) {}
 
+  /**
+   * @description Return all documents in a collection
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @returns {object}
+   *
+   * FirestoreCollectionResponse
+   * @example
+   * {
+   *    {
+   *     ok: boolean,
+   *     payload: documentId#1: {...documentData}
+   *     },
+   *    {
+   *     ok: boolean,
+   *     payload: documentId#2: {...documentData}
+   *     },
+   * ...
+   * }
+   */
   async getAll(collectionName: string): Promise<FirestoreCollectionResponse> {
     let result = {};
 
@@ -31,6 +55,14 @@ export default class FirestoreService implements FirestoreCRUDService {
     };
   }
 
+  /**
+   * @description Return a document by id
+   * If a document is not found, it is returned as null in the payload of the response
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @param {string} documentId
+   * @returns {object} - FirestoreDocumentResponse - {ok: boolean, payload: DocumentData | null, error: any}
+   */
   async getById(
     collectionName: string,
     documentId: string
@@ -51,12 +83,25 @@ export default class FirestoreService implements FirestoreCRUDService {
     }
   }
 
+  /**
+   * @description Add a document to the collection passed as argument
+   * If an error occurs, the error is returned in the payload of the response
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @param {object} data - The document data
+   * @returns {object}
+   *
+   * FirestoreAdditionSuccessResponse | FirestoreErrorResponse
+   * - *Success response*: {ok: boolean, payload: DocumentData}
+   * - *Error response*: {ok: false, error: any}
+   */
   async add(
     collectionName: string,
     data: { [key: string]: any }
-  ): Promise<FirestoreDocumentResponse> {
+  ): Promise<FirestoreAdditionSuccessResponse | FirestoreErrorResponse> {
     try {
       const docRef = await addDoc(collection(this.db, collectionName), data);
+
       return {
         ok: true,
         payload: docRef.id,
@@ -64,17 +109,28 @@ export default class FirestoreService implements FirestoreCRUDService {
     } catch (e) {
       return {
         ok: false,
-        payload: null,
         error: e,
       };
     }
   }
 
+  /**
+   * @description Update a document by id
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @param {string} documentId - The document id to update
+   * @param {object} updatedData - Data to update
+   * @returns {object}
+   *
+   * FirestoreSuccessResponse | FirestoreErrorResponse
+   * - *Success response*: {ok: boolean}
+   * - *Error response*: {ok: false, error: any}
+   */
   async update(
     collectionName: string,
     documentId: string,
     updatedData: any
-  ): Promise<FirestoreDocumentResponse> {
+  ): Promise<FirestoreSuccessResponse | FirestoreErrorResponse> {
     try {
       const docRef = doc(this.db, collectionName, documentId);
 
@@ -88,16 +144,26 @@ export default class FirestoreService implements FirestoreCRUDService {
     } catch (e) {
       return {
         ok: false,
-        payload: null,
         error: e,
       };
     }
   }
 
+  /**
+   * @description Delete a document by id
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @param {string} documentId - The document id to update
+   * @returns {object}
+   *
+   * FirestoreSuccessResponse | FirestoreErrorResponse
+   * - *Success response*: {ok: boolean}
+   * - *Error response*: {ok: false, error: any}
+   */
   async delete(
     collectionName: string,
     documentId: string
-  ): Promise<FirestoreDocumentResponse> {
+  ): Promise<FirestoreSuccessResponse | FirestoreErrorResponse> {
     try {
       await deleteDoc(doc(this.db, collectionName, documentId));
       return {
@@ -111,7 +177,20 @@ export default class FirestoreService implements FirestoreCRUDService {
     }
   }
 
-  async deleteAll(collectionName: string): Promise<FirestoreDocumentResponse> {
+  /**
+   * @description Delete all document in a collection
+   *
+   * @param {string} collectionName - The name of the Firestore collection
+   * @param {string} documentId - The document id to update
+   * @returns {object}
+   *
+   * FirestoreSuccessResponse | FirestoreErrorResponse
+   * - *Success response*: {ok: boolean}
+   * - *Error response*: {ok: false, error: any}
+   */
+  async deleteAll(
+    collectionName: string
+  ): Promise<FirestoreSuccessResponse | FirestoreErrorResponse> {
     try {
       const querySnapshot = await getDocs(collection(this.db, collectionName));
 
