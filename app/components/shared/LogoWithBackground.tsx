563ac27c95be71bc6b0a84e98c409623a3ea7587
Nicola <lanzoni.nicola@gmail.com>
2022-03-31 16:03:14 -0300

    Create Logo Component

---
+++ app/components/shared/LogoWithBackground.tsx
@@ -0,0 +1,17 @@
+import { Center, Image, Text } from "@chakra-ui/react";
+
+export default function LogoWithBackground() {
+  return (
+    <Center flexDirection="column" gap="1rem">
+      <Image src={`/images/thank-you.png`} />
+      <Text
+        fontSize="14px"
+        textTransform="uppercase"
+        letterSpacing="2px"
+        fontWeight="700"
+      >
+        Gustavo & Kelly
+      </Text>
+    </Center>
+  );
+}
