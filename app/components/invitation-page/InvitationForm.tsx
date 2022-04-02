08d600590fd3bc8b43f5aebeb2c8d671eeeeb8c2
Nicola <lanzoni.nicola@gmail.com>
2022-04-02 15:54:23 -0300

    Reorganizer folder

---
+++ app/components/invitation-page/InvitationForm.tsx
@@ -0,0 +1,90 @@
+import { Alert, AlertIcon, Button, Stack } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
+import { Form } from "remix";
+import { InvitationFormResponse } from "~/modules/invitations/models/invitation.model";
+import { RemixFormState } from "~/modules/shared/interfaces/RemixRun";
+import FormInputGuestName from "./components/FormInputGuestName";
+
+import FormInputUID from "./components/FormInputUID";
+import FormSelectGuests from "./components/FormSelectGuests";
+import FormSelectMealPreference from "./components/FormSelectMealPreference";
+import FormSelectWillAttend from "./components/FormSelectWillAttend";
+
+export default function InvitationForm({
+  formState,
+  actionData,
+  uid,
+}: {
+  formState: RemixFormState;
+  actionData: InvitationFormResponse;
+  uid: string;
+}) {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+  }, [formState]);
+
+  return (
+    <>
+      <Form method="post" ref={formRef}>
+        <Stack direction="column" spacing={8}>
+          <FormInputUID uid={uid} />
+
+          <FormInputGuestName />
+
+          <FormSelectWillAttend />
+
+          <Stack spacing={4}>
+            <FormSelectGuests />
+            <FormSelectMealPreference />
+          </Stack>
+          <Button
+            type="submit"
+            isLoading={formState === "submitting"}
+            loadingText="Enviando..."
+            bg="secondary.500"
+            disabled={formState === "success"}
+          >
+            {formState === "success" ? "Enviado" : "Envia"}
+          </Button>
+        </Stack>
+      </Form>
+      {actionData?.ok === false && (
+        <ErrorMessage message={"Erro! Por favor, tente novamente mais tarde"} />
+      )}
+    </>
+  );
+}
+
+function ErrorMessage({
+  message,
+  ...props
+}: {
+  message: string;
+  [key: string]: any;
+}) {
+  return (
+    <Alert status="error" {...props} mt="1.5rem">
+      <AlertIcon />
+      {message}
+    </Alert>
+  );
+}
+
+function SuccessMessage({
+  message,
+  ...props
+}: {
+  message: string;
+  [key: string]: any;
+}) {
+  return (
+    <Alert status="success" {...props} mt="1.5rem">
+      <AlertIcon />
+      {message}
+    </Alert>
+  );
+}
---
+++ app/components/invitation-page/components/FormHeader.tsx
@@ -0,0 +1,33 @@
+import { Flex, Center, Text } from "@chakra-ui/react";
+import BaseHeading from "~/components/shared/BaseHeadings";
+import EllipseDecorator from "~/components/shared/EllipseDecorator";
+import { TextDecorated } from "~/components/shared/TextDecorated";
+import settings from "~/modules/settings";
+
+export default function FormHeader() {
+  return (
+    <Flex direction="column" gap="1.5rem">
+      <Center>
+        <TextDecorated>Momentos mágicos</TextDecorated>
+      </Center>
+      <BaseHeading fontSize="38px" fontWeight="700" color="text.500">
+        Você vai estar presente em nosso dia especial?
+      </BaseHeading>
+      <Text fontSize="14px" textTransform="uppercase" letterSpacing="1px">
+        CASAMENTO EM{" "}
+        <Text as="span" fontWeight={700}>
+          {" "}
+          {settings.eventFullDateHumanReadable}
+        </Text>
+      </Text>
+      <Center w="100%" h="30px" gap="1rem">
+        <EllipseDecorator
+          diameter="8px"
+          repeat={3}
+          bg="primary.500"
+          gap="1rem"
+        />
+      </Center>
+    </Flex>
+  );
+}
