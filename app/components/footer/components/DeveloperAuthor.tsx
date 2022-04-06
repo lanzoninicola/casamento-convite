e67d46912c8daff4b043fa9dad18fb68c909dc0a
Nicola <lanzoni.nicola@gmail.com>
2022-04-06 16:04:42 -0300

    Move access to admin services from the footer to the DeveloperAuthor section

--- app/components/footer/components/DeveloperAuthor.tsx
+++ app/components/footer/components/DeveloperAuthor.tsx
@@ -1,10 +1,25 @@
 import { Center, VStack, HStack } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "remix";
 import { HeartIcon } from "~/components/shared/Icons";
 import FooterText from "./FooterText";
 
 export default function DeveloperAuthor() {
+  const [clickCounter, setClickCounter] = useState<number>(0);
+  let navigate = useNavigate();
+
+  function handleClicks() {
+    setClickCounter(clickCounter + 1);
+  }
+
+  useEffect(() => {
+    if (clickCounter > 5) {
+      navigate("/service");
+    }
+  }, [clickCounter]);
+
   return (
-    <Center bg="secondary.500" paddingBlock="1rem">
+    <Center bg="secondary.500" paddingBlock="1rem" onClick={handleClicks}>
       <VStack spacing={0}>
         <HStack>
           <FooterText>Made with</FooterText>
--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,7 +1,4 @@
 import { Box, Divider, Grid } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { useNavigate } from "remix";
-
 import LogoWebsite from "../shared/LogoWebsite";
 import DeveloperAuthor from "./components/DeveloperAuthor";
 import FooterCashGift from "./components/FooterCashGift";
@@ -9,27 +6,8 @@ import FooterMenu from "./components/FooterMenu";
 import FooterRightColumn from "./components/FooterRightColumn";
 
 export default function Footer() {
-  let navigate = useNavigate();
-  const [clickCounter, setClickCounter] = useState<number>(0);
-
-  function handleClicks() {
-    setClickCounter(clickCounter + 1);
-  }
-
-  useEffect(() => {
-    if (clickCounter > 5) {
-      navigate("/service");
-    }
-  }, [clickCounter]);
-
   return (
-    <Box
-      as="footer"
-      mt="4rem"
-      onClick={handleClicks}
-      bg="secondary.500"
-      padding="4rem 2rem"
-    >
+    <Box as="footer" mt="4rem" bg="secondary.500" padding="4rem 2rem">
       <Grid gridTemplateRows="repeat(7, auto)" gap="1rem">
         <LogoWebsite mb="2rem" />
         <Divider color="gray.200" />
