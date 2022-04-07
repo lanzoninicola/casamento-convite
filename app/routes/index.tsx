c284ab6b226d1d84729f5687b7b602a649262ac5
Nicola <lanzoni.nicola@gmail.com>
2022-04-06 22:02:31 -0300

    Added viewport

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -65,7 +65,7 @@ export default function Index() {
           <Footer />
           <AlertNoReadStory />
           <WhatsAppWidget />
-          {/* <ViewportInfo enableOnProduction={false} /> */}
+          <ViewportInfo enableOnProduction={true} />
         </PhotoGalleryProvider>
       </HistoryProvider>
     </>
