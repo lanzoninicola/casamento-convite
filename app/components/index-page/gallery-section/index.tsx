68bf3d99dce2983f72caa1d1823bafec3eb03c10
Nicola <lanzoni.nicola@gmail.com>
2022-04-07 20:22:57 -0300

    Fix photo gallery overflow

--- app/components/index-page/gallery-section/index.tsx
+++ app/components/index-page/gallery-section/index.tsx
@@ -24,6 +24,7 @@ export default function Gallery() {
           h="auto"
           bg="primary.500"
           paddingBlock="4rem"
+          overflow="hidden"
         >
           {!isPhotoZoomed && (
             <Flex direction="column" gap=".5rem" justify="center" h="100%">
