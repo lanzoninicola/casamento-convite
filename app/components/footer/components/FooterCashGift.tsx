21dde9266a0e0553b781d134ffbc6bd55dd4dec2
Nicola <lanzoni.nicola@gmail.com>
2022-04-06 21:42:28 -0300

    Update layout footer

--- app/components/footer/components/FooterCashGift.tsx
+++ app/components/footer/components/FooterCashGift.tsx
@@ -1,4 +1,4 @@
-import { Grid, HStack } from "@chakra-ui/react";
+import { Flex, Grid, HStack } from "@chakra-ui/react";
 import { EnvelopeIcon, GiftIcon, PixIcon } from "~/components/shared/Icons";
 import FooterButton from "./FooterButton";
 import FooterHeading from "./FooterHeading";
@@ -6,22 +6,24 @@ import FooterHeading from "./FooterHeading";
 export default function FooterCashGift() {
   return (
     <>
-      <HStack>
-        <GiftIcon />
-        <FooterHeading>Presente</FooterHeading>
-      </HStack>
-      <Grid gridTemplateColumns="1fr 1fr" gap="1rem">
-        <FooterButton
-          leftIcon={<EnvelopeIcon size={24} />}
-          text="ENVELOPE"
-          to="/invitation/gift/envelope"
-        />
-        <FooterButton
-          leftIcon={<PixIcon size={24} />}
-          text="PIX"
-          to="/invitation/gift/pix"
-        />
-      </Grid>
+      <Flex direction="column" justify="center" gap="1rem">
+        <HStack>
+          <GiftIcon />
+          <FooterHeading>Presente</FooterHeading>
+        </HStack>
+        <Grid gridTemplateColumns="1fr 1fr" gap="1rem">
+          <FooterButton
+            leftIcon={<EnvelopeIcon size={24} />}
+            text="ENVELOPE"
+            to="/invitation/gift/envelope"
+          />
+          <FooterButton
+            leftIcon={<PixIcon size={24} />}
+            text="PIX"
+            to="/invitation/gift/pix"
+          />
+        </Grid>
+      </Flex>
     </>
   );
 }
