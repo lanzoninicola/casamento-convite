d5804c861c0d940145fffd5cca39d439e8d82595
Nicola <lanzoni.nicola@gmail.com>
2022-05-02 20:44:54 -0300

    Change invitation confirmation due date

--- app/modules/settings/index.ts
+++ app/modules/settings/index.ts
@@ -2,8 +2,8 @@ const settings = {
   eventDate: "2022-05-21",
   eventDateHumanReadable: "21.05.2022",
   eventFullDateHumanReadable: "21 Maio 2022",
-  invitationConfirmationWithin: "2022-04-30",
-  invitationConfirmationWithinHumanReadable: "30.04.2022",
+  invitationConfirmationWithin: "2022-05-08",
+  invitationConfirmationWithinHumanReadable: "08.05.2022",
   lunchPlaceAddress:
     "Chacara Zanin, Linha São Braz São, Estr. Mun. Lisando São Braz, Pato Branco - PR, 85504-000",
   app: {
